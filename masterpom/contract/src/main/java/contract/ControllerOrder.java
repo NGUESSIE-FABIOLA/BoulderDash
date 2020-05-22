8866c576091ba2811c403db893c527da1956e32a
lemeyou <58194255+lemeyou@users.noreply.github.com>
2020-05-22 11:18:00 +0100

    ok

--- masterpom/contract/src/main/java/contract/ControllerOrder.java
+++ masterpom/contract/src/main/java/contract/ControllerOrder.java
@@ -1,18 +1,18 @@
 package contract;
 
+
 /**
- * The Enum ControllerOrder.
+ * @author bryan
  *
- * @author Jean-Aymeric Diet
  */
+
 public enum ControllerOrder {
 
-	/** The English. */
-	English,
-	/** The French. */
-	Francais,
-	/** The Deutch. */
-	Deutsch,
-	/** The Indonesia. */
-	Indonesia
+
+	UP,
+	DOWN,
+	LEFT,
+	RIGHT,
+	CARRY,
+	NOP;
 }
--- masterpom/view/src/main/java/view/View.java
+++ masterpom/view/src/main/java/view/View.java
@@ -4,6 +4,7 @@ import java.awt.Dimension;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.SwingUtilities;
 
@@ -14,26 +15,27 @@ import contract.IModel;
 import contract.IMotionFullElement;
 import contract.IOrderPerformer;
 import contract.IView;
-import contract.Order;
-import fr.exia.showboard.BoardFrame;
+import fr.exia.showboard.*;
 
-
-/**
- * The Class View.
+ /**
+ * @author bryan
  *
- * @author Jean-Aymeric Diet
  */
-public final class View implements IView, Runnable {
 
+public final class View implements IView, Runnable{
+
+	
 	private int SIZESPRITE = 16;
 	
-	private int VIEWSIZE = SIZESPRITE * 15;
+	//private int VIEWSIZE = SIZESPRITE * 15;
 
     private Rectangle closeView;
     
     private IMap map; 
     
-    private IMotionFullElement mainCharacter;
+   // private IMotionFullElement mainCharacter;
+    
+    private IMotionFullElement character;
     
     private Point characterPosition;
     
@@ -56,9 +58,13 @@ public final class View implements IView, Runnable {
 	 *          the model
 	 */
 	public View(final IModel model) {
+		
 		this.viewFrame = new ViewFrame(model);
-		// this.getMyVehicle().getSprite().loadImage();
-		// this.setCloseView(new Rectangle(0, this.getMyVehicle().getY(), this.getRoad().getWidth(), roadView));
+		this.setMap(map);
+
+		this.setCharacter(character);
+		this.getCharacter().getSprite().loadImage();
+		this.setCloseView(new Rectangle(0, this.getCharacter().getY(), mapViewSizeX, mapViewSizeY));
 		SwingUtilities.invokeLater(this);
 	}
 
@@ -72,31 +78,63 @@ public final class View implements IView, Runnable {
 	
 	public  void show(){
 		
+		// // TODO Remplir la fonction show qui ne renvoie rien, mais print les
+				// // sprites
+				// System.out.println("afficher");
+				// int y = yStart % this.getMap().getHeight();
+				// for (int view = 0; view < this.getViewMapX(); view++) {
+				// for (int x = 0; x < this.getViewMapY(); x++) {
+				// if ((x == this.getMainCharacter().getX()) && (y == yStart)) {
+				// System.out.print(this.getMainCharacter().getSprite().getConsoleImage());
+				// } else {
+				// System.out.print(this.getMap().getElementByPosition(x,
+				// y).getSprite().getConsoleImage());
+				// }
+				// }
+				// y = (y + 1) % this.getMap().getHeight();
+				// System.out.print("\n");
+				// }
 	}
 	
 	public  void cameraMove() {
 		
+		int x;
+		int y;
+		x = (int) this.getCharacter().getX();
+		y = (int) this.getCharacter().getY();
+		if (x < this.getCloseView().x + 5) {
+			this.getCloseView().x--;
+		} else if (x > this.getCloseView().x + 10) {
+			this.getCloseView().x++;
+		}
+		if (y < this.getCloseView().y + 4) {
+			this.getCloseView().y--;
+		} else if (y > this.getCloseView().y + 10) {
+			this.getCloseView().y++;
+		}
 	}
 	
-	private Order keyCodeToUserOrder(int keyCode) {
-		return null;
-		
-		
-	}
-	
-	// ON POURRA SUPPRIMER CETTE METHODE AVEC LA CLASSE CONTROLLERORDER//
 	protected static ControllerOrder keyCodeToControllerOrder(final int keyCode) {
+		
 		switch (keyCode) {
-			case KeyEvent.VK_G:
-				return ControllerOrder.English;
-			case KeyEvent.VK_F:
-				return ControllerOrder.Francais;
-			case KeyEvent.VK_D:
-				return ControllerOrder.Deutsch;
-			case KeyEvent.VK_I:
-				return ControllerOrder.Indonesia;
+		
+			case KeyEvent.VK_RIGHT:
+				return ControllerOrder.RIGHT;
+				
+			case KeyEvent.VK_LEFT:
+				return ControllerOrder.LEFT;
+				
+			case KeyEvent.VK_UP:
+				return ControllerOrder.UP;
+				
+			case KeyEvent.VK_DOWN:
+				return ControllerOrder.DOWN;
+				
+			case KeyEvent.VK_C:
+				return ControllerOrder.CARRY;
+				
 			default:
-				return ControllerOrder.English;
+				return ControllerOrder.NOP;
 		}
 	} 
 
@@ -105,6 +143,7 @@ public final class View implements IView, Runnable {
 	 *
 	 * @see contract.IView#printMessage(java.lang.String)
 	 */
+	
 	public void printMessage(final String message) {
 		this.viewFrame.printMessage(message);
 	}
@@ -114,24 +153,38 @@ public final class View implements IView, Runnable {
 	 *
 	 * @see java.lang.Runnable#run()
 	 */
+	
 	public void run() 
 	{
 		this.viewFrame.setVisible(true);
 		
-		board = new BoardFrame("Close view");
-       // boardFrame.setDimension(new Dimension(this.getRoad().getWidth(), this.getRoad().getHeight()));
-        board.setDisplayFrame(this.closeView);
-        board.setSize(this.closeView.width * VIEWSIZE, this.closeView.height * VIEWSIZE);
-        board.setHeightLooped(true);
-       // boardFrame.addKeyListener(this);
-        board.setFocusable(true);
-        board.setFocusTraversalKeysEnabled(false);
+       final BoardFrame boardFrame = new BoardFrame("BoulderDash");
+		boardFrame.setDimension(new Dimension(this.getMap().getWidth(), this.getMap().getHeight()));
+		boardFrame.setDisplayFrame(closeView);
+		boardFrame.setSize(this.closeView.width * SIZESPRITE * 2, this.closeView.height * SIZESPRITE * 2);
+		//boardFrame.addKeyListener(this);
+		boardFrame.setFocusable(true);
+		boardFrame.setFocusTraversalKeysEnabled(false);
 
-       // boardFrame.addPawn(this.getMyVehicle());
+		for (int x = 0; x < this.getMap().getWidth(); x++) {
+			for (int y = 0; y < this.getMap().getHeight(); y++) {
+				this.map.getElementByPosition(x, y).setX(x);
+				this.map.getElementByPosition(x, y).setY(y);
 
-        //this.getRoad().getObservable().addObserver(boardFrame.getObserver());
+				boardFrame.addSquare((ISquare) this.map.getElementByPosition(x, y), x, y);
+				// boardFrame.addPawn((IPawn)this.getMap().getElementByPosition(x,
+				// y));
+				boardFrame.addPawn((IPawn) this.map.getElementByPosition(x, y));
+			}
+		}
 
-        board.setVisible(true);
+		boardFrame.addPawn(this.getCharacter());
+
+		this.getMap().getObservable().addObserver(boardFrame.getObserver());
+
+		boardFrame.setVisible(true);
+
+		setBoard(boardFrame);
 	}
 
 	/**
@@ -169,21 +222,27 @@ public final class View implements IView, Runnable {
 	 * @param map the map to set
 	 */
 	public void setMap(IMap map) {
+		
 		this.map = map;
+		for (int y = 0; y < this.getMap().getHeight(); y++) {
+			for (int x = 0; x < this.getMap().getWidth(); x++) {
+				this.getMap().getElementByPosition(x, y).getSprite().loadImage();
+			}
+		}
 	}
 
 	/**
 	 * @return the mainCharacter
 	 */
-	public IMotionFullElement getMainCharacter() {
-		return mainCharacter;
+	public IMotionFullElement getCharacter() {
+		return character;
 	}
 
 	/**
 	 * @param mainCharacter the mainCharacter to set
 	 */
-	public void setMainCharacter(IMotionFullElement mainCharacter) {
-		this.mainCharacter = mainCharacter;
+	public void setCharacter(IMotionFullElement character) {
+		this.character = character;
 	}
 
 	/**
@@ -257,6 +316,32 @@ public final class View implements IView, Runnable {
 	}
 	
 	public void updateboard() {
+		
+		BoardFrame boardFrame = new BoardFrame("BoulderDash");
+		boardFrame.setDimension(new Dimension(this.getMap().getWidth(), this.getMap().getHeight()));
+		boardFrame.setDisplayFrame(closeView);
+		boardFrame.setSize(this.closeView.width * SIZESPRITE * 2, this.closeView.height * SIZESPRITE * 2);
+		//boardFrame.addKeyListener(this);
+		boardFrame.setFocusable(true);
+		boardFrame.setFocusTraversalKeysEnabled(false);
+
+		for (int x = 0; x < this.getMap().getWidth(); x++) {
+			for (int y = 0; y < this.getMap().getHeight(); y++) {
+				this.map.getElementByPosition(x, y).setX(x);
+				this.map.getElementByPosition(x, y).setY(y);
+
+				boardFrame.addSquare((ISquare) this.map.getElementByPosition(x, y), x, y);
+				// boardFrame.addPawn((IPawn)this.getMap().getElementByPosition(x,
+				// y));
+				boardFrame.addPawn((IPawn) this.map.getElementByPosition(x, y));
+			}
+		}
+
+		boardFrame.addPawn(this.getMainCharacter());
+
+		this.getMap().getObservable().addObserver(boardFrame.getObserver());
+
+		boardFrame.setVisible(true);
 		
 	}
 }
