bdb9d1991ce022a48c090d506094d8c64042624f
Heinkek-99 <44591037+Heinkek-99@users.noreply.github.com>
2020-05-24 20:27:33 +0200

    Modificate Class Model and Character

--- masterpom/model/src/main/java/model/Model.java
+++ masterpom/model/src/main/java/model/Model.java
@@ -1,10 +1,12 @@
 package model;
 
+import java.sql.SQLException;
 import java.util.Observable;
 
 import contract.IModel;
 import contract.IMotionFullElement;
 import model.element.Map;
+import model.element.motionfull.Character;
 
 /**
  * The Class Model.
@@ -12,14 +14,49 @@ import model.element.Map;
  * @author Jean-Aymeric Diet
  */
 public final class Model implements IModel {
-
+	
+	/** The map*/
 	private Map map;
 
-	@Override
+	/** The player*/
+	private IMotionFullElement character;
+	public Model(int i) throws SQLException {
+		// TODO Auto-generated constructor stub
+		this.setMap(new Map(i));
+	     this.setCharacter(new Character(getMap()) );
+		// this.setCharacter(new Character(getMap())); // y5 x7 TODO get rid of the hard code
+	       this.character.setX(7);
+	       this.character.setY(5);
+	}
+
+
+
+	/**
+	 * @return the map
+	 */
 	public Map getMap() {
-		// TODO Auto-generated method stub
 		return this.map;
 	}
+
+
+
+	/**
+	 * @param map the map to set
+	 */
+	public void setMap(Map map) {
+		this.map = map;
+	}
+
+
+
+	/**
+	 * @param character the character to set
+	 */
+	public void setCharacter(IMotionFullElement character) {
+		this.character = character;
+	}
+
+
 
 	@Override
 	public IMotionFullElement getCharacter() {
--- masterpom/model/src/main/java/model/element/motionfull/Character.java
+++ masterpom/model/src/main/java/model/element/motionfull/Character.java
@@ -3,7 +3,7 @@
  */
 package model.element.motionfull;
 
-import java.io.IOException;
+import java.awt.image.BufferedImage;
 
 import contract.Permeability;
 import contract.Sprite;
@@ -15,118 +15,37 @@ import model.element.Map;
  */
 public class Character extends MotionFullElement{
 
-    /** The Constant SPRITE. */
-    private static final Sprite sprite      = new Sprite('H', "R1.png");
+	private static final Sprite CHARACTER = new Sprite('H');
+	
+	BufferedImage buffer;
+	
 
-    /** The Constant spriteUp. */
-    private static final Sprite spriteUp   = new Sprite('H', "2.png");
-    
-    /** The Constant spriteTurnLeft. */
-    private static final Sprite spriteLeft  = new Sprite('H', "A1.png");
-
-    /** The Constant spriteTurnRight. */
-    private static final Sprite spriteRight = new Sprite('H', "D1.png");
-
-    /** The Constant spriteDie. */
-    private static final Sprite spriteDie   = new Sprite('H', "Death.png");
-
-    /** The Constant spriteDown. */
-    private static final Sprite spriteDown   = new Sprite('H', "U1.png");
-	  /** The Constant SPRITE. */
-    
-    private static final Sprite spriteFight = new Sprite('H', "F1.png");
-
-	private Map map;
-
-	private boolean alive;
-
-    /**
-     * Instantiates a new Character.
-     * @throws IOException 
-     */
-    
-    public Character(final int x, final int y) throws IOException {
-        super(x, y, sprite, Permeability.BLOCKING);
-        spriteUp.loadImage();
-        spriteDown.loadImage();
-        spriteLeft.loadImage();
-        spriteRight.loadImage();
-        spriteFight.loadImage();
-        spriteDie.loadImage();
-    }
-
-   
-    public Character(Map map) {
-		// TODO Auto-generated constructor stub
-    	super(map);
-    	this.alive = true;
+	
+	
+	public Character(Map map){
+		super(CHARACTER, Permeability.BLOCKING, map);
+		this.alive =true;
 	}
 
-
-	/*
-     * (non-Javadoc)
-=     */
-    @Override
-    public final void moveUp() {
-        super.moveLeft();
-        this.setSprite(spriteUp);
-    }
-    /**
+	
+	/**
+	 * Checks if is alive.
      *
+     * @return  alive
      */
-    @Override
+	@Override
 	public Boolean isAlive() {
 		return this.alive;
 	}
-    /*
-     * (non-Javadoc)
-=     */
-    @Override
-    public final void moveDown() {
-        super.moveLeft();
-        this.setSprite(spriteDown);
-    }
-    
-    /*
-     * (non-Javadoc)
-=     */
-    @Override
-    public final void Fight() {
-        super.moveLeft();
-        this.setSprite(spriteFight);
-    }
-    
-    /*
-     * (non-Javadoc)
-=     */
-    @Override
-    public final void moveLeft() {
-        super.moveLeft();
-        this.setSprite(spriteLeft);
-    }
 
-    /*
-     * (non-Javadoc)
-=     */
-    @Override
-    public final void moveRight() {
-        super.moveRight();
-        this.setSprite(spriteRight);
-    }
 
-    /*
-     * (non-Javadoc)
-=     */
-    @Override
-	public
-    final void die() {
-        super.die();
-        this.setSprite(spriteDie);
-    }
+	/**
+	 * Sets alive to false
+	 */
+	@Override
+	public void die() {
+		this.alive = false;
+		
+	}
 
-    @Override
-    public final void doNothing() {
-        super.doNothing();
-        this.setSprite(sprite);
-    }
 }
