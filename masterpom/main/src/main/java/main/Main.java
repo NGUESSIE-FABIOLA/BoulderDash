f2f835bae82843d4f61b20fb0964d9d475173b87
Heinkek-99 <44591037+Heinkek-99@users.noreply.github.com>
2020-06-02 15:36:04 +0200

    End

--- masterpom/main/src/main/java/main/Main.java
+++ masterpom/main/src/main/java/main/Main.java
@@ -1,5 +1,6 @@
 /**
- * 
+ * @author Jean-Aymeric DIET jadiet@cesi.fr
+ * @version 1.0
  */
 package main;
 
@@ -13,6 +14,7 @@ import controller.IController;
 import model.IModel;
 import model.Model;
 import view.View;
+//import view.ViewFacade;
 
 /**
  * The Class Main.
@@ -26,29 +28,31 @@ public abstract class Main {
      *
      * @param args
      *            the arguments
-	 * @throws SQLException
-	 * 			SQL error
-	 * @throws IOException
-	 * 			reject exception
-	 */
-	public static void main(final String[] args) throws SQLException, IOException {
-	    	
-		 Object[] levelchoice = {1, 2, 3, 4, 5, 6};
-			
-			int level = (int)JOptionPane.showInputDialog(null, "Choose your level ", "Boulderdash", JOptionPane.QUESTION_MESSAGE, null, levelchoice, levelchoice[0]);
-			
-			JOptionPane.showMessageDialog(null, "Vous avez choisi le niveau " + level, null, JOptionPane.INFORMATION_MESSAGE);
-			
-	    	final IModel model = new Model(level);
-	    	final View view = new View(model.getMap(), model.getCharacter(), model.getMap().getPawns());
-	        final IController controller = new Controller(view, model);
-	        view.setOrderPerformer(controller.getOrderPeformer());
+     * @throws IOException 
+     * 			Input output error
+     * @throws InterruptedException
+     * 			interrupted 
+     * @throws SQLException
+     * 			SQL error 
+     */
+    public static void main(final String[] args) throws IOException, SQLException, InterruptedException {
+    	
+    	
+    	Object[] levelchoice = {1, 2, 3, 4, 5};
+		
+		int level = (int)JOptionPane.showInputDialog(null, "Choose your level", "BoulderDash", JOptionPane.QUESTION_MESSAGE, null, levelchoice, levelchoice[0]);
+	
+		JOptionPane.showMessageDialog(null, "Vous avez choisi le niveau" + level, null, JOptionPane.INFORMATION_MESSAGE);
+		final IModel model = new Model(level);
+    	final View view = new View(model.getMap(), model.getCharacter(), model.getMap().getPawns());
+        final IController controller = new Controller(view, model);
+        view.setOrderPerformer(controller.getOrderPeformer());
 
-	        try {
-	            controller.start();
-	        } catch (InterruptedException e) {
-	        	System.out.println(e.getMessage());
-				e.printStackTrace();
-			}
-	    }
+        try {
+            controller.start();
+        } catch (InterruptedException e) {
+        	System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+    }
 }
